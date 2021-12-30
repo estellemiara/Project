6fbe663624d49b50e066c6d0cca0f14057ca4d35
estellemiara <73658281+estellemiara@users.noreply.github.com>
2021-12-30 16:44:28 +0200

    Update read_image.m

--- Project2/read_image.m
+++ Project2/read_image.m
@@ -1,7 +1,7 @@
 function read_image(img)
     file_name=strcat(img, '.jpeg');
     disp(file_name);
-    image=imread(file_name);
+    image=imread(file_name); %read image that holds the name "img.jpeg" 
     figure(6);
     imshow(image);
     title(img);
