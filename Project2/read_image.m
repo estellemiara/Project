df30c15237a56226e6ee0b85c96147ab54243a22
estellemiara <73658281+estellemiara@users.noreply.github.com>
2022-01-03 21:52:26 +0200

    Update read_image.m

--- Project2/read_image.m
+++ Project2/read_image.m
@@ -1,7 +1,7 @@
 function read_image(img)
-    file_name=strcat(img, '.jpeg');
+    file_name=strcat(img, '.jpg');
     disp(file_name);
-    image=imread(file_name); %read image that holds the name "img.jpeg" 
+    image=imread(file_name); %read image that holds the name "img.jpg" 
     figure(6);
     imshow(image);
     title(img);
